3e0a4799f39052ca0966616a90a8c553d6edf6bc
Marcel Schneider <marcel@coopmasters.de>
2019-08-07 22:32:36 +0200

    one overload for operator|() only
    all cases are handled by using "if constexpr"

--- include/result_type.h
+++ include/result_type.h
@@ -20,7 +20,7 @@ namespace ResultType {
         Success, Error
     };
     template<class T>
-    using is_result_type = is_detected<detail::has_Success, T> ;
+    using is_result_type = is_detected<detail::has_Success, T>;
 
     template<class SuccessType, class ErrorType>
     class Result {
@@ -52,14 +52,15 @@ namespace ResultType {
             return std::get<SuccessType>(result_type_value);
         }
 
-        constexpr SuccessType const& CRefSuccess() const&{
+        constexpr SuccessType const &CRefSuccess() const &{
             return std::get<SuccessType>(result_type_value);
         }
 
         constexpr ErrorType &RefError() &{
             return std::get<ErrorType>(result_type_value);
         }
-        constexpr ErrorType const& CRefError() const&{
+
+        constexpr ErrorType const &CRefError() const &{
             return std::get<ErrorType>(result_type_value);
         }
 
@@ -78,40 +79,37 @@ namespace ResultType {
         return result.State() == ResultState::Error;
     }
 
-    template<typename T, typename F>
-    constexpr auto operator|(T &&t, F f) {
-        return f(std::forward<T>(t));
-    }
 
-    template<typename T, typename E, typename Callee>
-    constexpr auto operator|(Result<T, E> &&arg, Callee &&f) {
-        using ArgType = Result<T, E>;
-        using ArgSuccessType = decltype(std::forward<ArgType>(arg).Success());
+    template<typename ArgType, typename Callee>
+    constexpr auto operator|(ArgType &&arg, Callee &&f) {
+        if constexpr (is_result_type<ArgType>::value) {
+            using ArgSuccessType = decltype(std::forward<ArgType>(arg).Success());
+            using ArgErrorType   = decltype(std::forward<ArgType>(arg).Error());
 
-        static_assert(std::is_invocable_v<Callee, ArgType> || std::is_invocable_v<Callee, T>);
+            static_assert(std::is_invocable_v<Callee, ArgType> || std::is_invocable_v<Callee, ArgSuccessType>);
 
-        if constexpr (std::is_invocable_v<Callee, ArgType>){
-            return f(std::forward<ArgType>(arg));
-        }
-        else if constexpr (std::is_invocable_v<Callee, T>){
-            using callee_return_type = decltype(f(std::declval<ArgSuccessType>()));
-            if constexpr (is_result_type<callee_return_type>::value) {
-                return IsSuccess(arg) ? f(std::forward<ArgType>(arg).Success())
-                                         : callee_return_type(std::forward<ArgType>(arg).Error());
-            }
-            else{
-                if constexpr (std::is_void_v<callee_return_type>){
-                    if (IsSuccess(arg)){
-                        f(std::forward<ArgType>(arg).Success());
+            if constexpr (std::is_invocable_v<Callee, ArgType>) {
+                return f(std::forward<ArgType>(arg));
+            } else if constexpr (std::is_invocable_v<Callee, ArgSuccessType>) {
+                using callee_return_type = decltype(f(std::declval<ArgSuccessType>()));
+                if constexpr (is_result_type<callee_return_type>::value) {
+                    return IsSuccess(arg) ? f(std::forward<ArgType>(arg).Success())
+                                          : callee_return_type(std::forward<ArgType>(arg).Error());
+                } else {
+                    if constexpr (std::is_void_v<callee_return_type>) {
+                        if (IsSuccess(arg)) {
+                            f(std::forward<ArgType>(arg).Success());
+                        }
+                    } else {
+                        using ReturnType = Result<callee_return_type, typename std::remove_reference<ArgErrorType>::type>;
+                        return IsSuccess(arg)
+                               ? ReturnType(f(std::forward<ArgType>(arg).Success()))
+                               : ReturnType(std::forward<ArgType>(arg).Error());
                     }
                 }
-                else {
-                    using ReturnType = Result<callee_return_type, E>;
-                    return IsSuccess(arg)
-                        ? ReturnType(f(std::forward<ArgType>(arg).Success()))
-                        : ReturnType(std::forward<ArgType>(arg).Error());
-                }
             }
+        } else {
+            return f(std::forward<ArgType>(arg));
         }
     }
 }
