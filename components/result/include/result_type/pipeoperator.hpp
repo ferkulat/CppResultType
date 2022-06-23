4fcdee281a1169218d26b900171a0da9e9dc9bda
Marcel Schneider <marcel@coopmasters.de>
2022-06-23 20:54:45 +0200

    remove cpp17 implementation
    no need for double maintainance. cpp14 works as well.

--- components/result/include/result_type/pipeoperator.hpp
+++ components/result/include/result_type/pipeoperator.hpp
@@ -5,9 +5,5 @@
 #ifndef CPPRESULTTYPE_PIPEOPERATOR_H
 #define CPPRESULTTYPE_PIPEOPERATOR_H
 #include <result_type/optional.hpp>
-#ifdef CPPRESULTTYPE_WITH_CPP14_PIPE
 #include <result_type/pipeoperator_cpp14.hpp>
-#else
-#include <result_type/pipeoperator_cpp17.hpp>
-#endif
 #endif //CPPRESULTTYPE_PIPEOPERATOR_H
